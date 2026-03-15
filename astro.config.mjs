b990da71bb1f1eb80c7eea4dccbe0cb8924928d5
pranney82 <peter@contractorcto.com>
2026-03-15 16:40:07 -0400

    morez

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://sunnyandranney.com',
-  output: 'hybrid',
+  output: 'static',
   adapter: cloudflare(),
   integrations: [
     sitemap(),
